ea86e6d250a45a7b52d6fb8f20b3d4510a91cb75
User <user@Users-MacBook-Air.local>
2021-05-05 12:51:39 -0700

    new testing functions in test_human and test_tribe

--- test/test_human.py
+++ test/test_human.py
@@ -1,11 +1,15 @@
 '''
 	Test Suite for test all components of 'Human'.
 '''
+from unittest.mock import patch
 
 from humans.human import Human
 
 class TestHumanCreation:
-	# Check the behavior of the default Human constructor
-	def test_default_human_creation(self):
+	
+	# @patch allows to test abstract classes without overriding abstract methods.
+	@patch("test_human.Human.__abstractmethods__", set())
+	def test_human_has_default_inventory(self):
 		test_human = Human()
-		assert('Unknown', None) == (test_human.name, test_human.age)
+		for k,v in test_human.inventory.items():
+			assert(v == None)
--- test/test_tribe.py
+++ test/test_tribe.py
@@ -1,6 +1,8 @@
 '''
 Test Suite for testing all components of a 'tribe'.
 '''
+from unittest.mock import patch
+
 from tribe.tribe import Tribe
 from humans.human import Human
 
@@ -20,11 +22,12 @@ class TestTribeCreation:
 class TestTribeFunctions:
 	
 	# Checks the behavior of adding a Human to an existing role in tribe.roles_directory
-	def test_adding_human_to_role(self):
-		test_human = Human('Samuel', 23)
+	@patch("test_human.Human.__abstractmethods__", set())
+	def test_adding_to_role(self):
+		test_human = Human()
 		test_human_identifier = test_human.get_human_identifier()
 		test_tribe = Tribe()
-		test_tribe.add_human_to_role(test_human, 'base')
+		test_tribe.add_to_role(test_human, 'base')
 		assert test_human.human_identifier in test_tribe.roles_directory.values()
 
 
