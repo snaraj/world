8688d2b84f25fef019ca0736ff018cb8680758d7
User <user@Users-MacBook-Air.local>
2021-05-05 12:49:43 -0700

    Made human into an abstract base class that various roles can inherit from

--- humans/human.py
+++ humans/human.py
@@ -2,55 +2,53 @@
 Defines the blueprint for a human.
 '''
 from abc import ABC, abstractmethod
+
 import uuid
+
+from .inventory.inventory import Inventory
 
 class Human(ABC):
 
 	def __init__(self):
 		self.human_identifier = str(uuid.uuid4().int)
-		self.human_attribute_tree = self.HumanAttributeTree()
+		self.human_attribute_tree = self.HumanAttributeTree().human_attribute_tree_dictionary
+		self.inventory = Inventory().content
 
-	def __str__(self):
-		format_dictionary = {'name' : self.name,
-							'age': self.age,
-							'human_identifier' : self.human_identifier}
+	# Abstract getters common across all humans
+	@abstractmethod
+	def get_name(self):
+		pass
 
-		return 'Human: #{human_identifier} - Name: {name} - Age: {age}'.format(**format_dictionary)
+	@abstractmethod
+	def get_age(self):
+		pass
 
-	def get_human_name(self):
-		return self.name
-
-	def get_human_age(self):
-		return self.age
+	@abstractmethod
+	def add_to_inventory(self):
+		pass
 
 	def get_human_identifier(self):
 		return self.human_identifier
 
-	def get_name_from_human_identifier(self, human_identifier):
-		return 
-
 	class HumanAttributeTree():
 
-		_human_attribute_tree_dictionary = {
+		human_attribute_tree_dictionary = {
 			'intelligence' : 0,
 			'strength': 0
 		}
 
 		def __init__(self, 
-					intelligence = _human_attribute_tree_dictionary['intelligence'], 
-					strength = _human_attribute_tree_dictionary['strength']):
+					intelligence = human_attribute_tree_dictionary['intelligence'], 
+					strength = human_attribute_tree_dictionary['strength']):
 
 			self.intelligence = intelligence
 			self.strength = strength
 
 		def __str__(self):
-			return 'Displaying known human attribute tree: \n intelligence: {intelligence} - strength: {strength}'.format(**self._human_attribute_tree_dictionary)
+			return 'Displaying human attribute tree: \n intelligence: {intelligence} - strength: {strength}'.format(**self._human_attribute_tree_dictionary)
 
 		def __getitem__(self, attribute):
 			try: 
-				return self._human_attribute_tree_dictionary[attribute]
+				return self.human_attribute_tree_dictionary[attribute]
 			except:
 				return 'Attribute does not exist.'
-
-human = Human()
-print(human.human_attribute_tree['intelligence'])
