db9fc6c75021413e875646989d9d29d1e28a1fd6
User <user@Users-MacBook-Air.local>
2021-05-06 13:04:30 -0700

    adding the ability to connect to a local postgreSQL database. No models implemented yet.

--- config/settings/base.py
+++ config/settings/base.py
@@ -2,25 +2,49 @@
 Base settings to build other settings files upon.
 """
 
-from pathlib import Path
+import psycopg2
+from psycopg2 import OperationalError
 
-import sqlite3
+from pathlib import Path
 
 # Grab the path to the ROOT_DIR
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
-# Define connection (db name)
-connection = sqlite3.connect('civilization.db')
-# Define cursor, used to interact with the db through sql commands.
-cursor = connection.cursor()
+# Connecting to the database
+def create_connection(name, user, password, host, port):
+	# Creating the pointer object for interacting with the db.
+	connection = None
+	try:
+		connection = psycopg2.connect(
+				database = name,
+				user = user,
+				password = password,
+				host = host,
+				port = port
+			)
+		print(f'Connection to database: {name} is succesful.')
+	except OperationalError as e:
+		print(f'The error {e} occured.')
+	#returns the pointer object.
+	return connection
 
-# Creating a basic Human Table.
-create_human_table = """CREATE TABLE IF NOT EXISTS
-human(human_identifier TEXT PRIMARY KEY, name VARCHAR, age VARCHAR)"""
-cursor.execute(create_human_table)
+# Executing queries to the DB.
+def execute_query(connection, query):
+	connection.autocommit = True
+	cursor = connection.cursor()
+	try:
+		# Executes the given query
+		cursor.execute(query)
+		print(f'Query {query} was succesfully executed.')
+	except OperationalError as e:
+		print(f'The error{e} occured.')
 
-# Creating a basic Tribe Table.
-create_tribe_table = """CREATE TABLE IF NOT EXISTS
-tribe(tribe_identifier INTEGER PRIMARY KEY, name TEXT, population VARCHAR,
-name_of_founder TEXT, roles_directory TEXT)"""
-cursor.execute(create_tribe_table)
+
+
+
+
+
+
+
+
+
