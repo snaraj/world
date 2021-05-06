6b32a576f036db0a22f32c37cb7d962e56c0d871
User <user@Users-MacBook-Air.local>
2021-05-06 12:37:50 -0700

    adding buy_item(), which supports to ability to purchase an item from the a shop. The item can only be purchased if the item is in stock. this method is not checking whether the player has avaliable funds yet.

--- industries/retail/shop.py
+++ industries/retail/shop.py
@@ -1,25 +1,51 @@
 '''
 	Blueprint for a shop
+
+	The catalog for the shop follows this logic:
+		{'item name' : [quantity avaliable, item price]}
 '''
 class Shop:
 
-	def __init__(self, name, inventory):
+	def __init__(self, name, catalog):
 		self.name = name
-		self.inventory = inventory
+		self.catalog = catalog
 
 	def __str__(self):
-		format_dictionary = {'name' : self.name, 'inventory' : self.inventory}
-		return 'Shop: {name} \n Inventory: {inventory}'.format(**format_dictionary)
+		return f'Shop: {self.name} \n catalog: {self.catalog}'
 
-	def get_inventory(self):
-		return self.inventory
+	# returns the catalog of the shop
+	def get_catalog(self):
+		return self.catalog
 
+	# returns the name of the shop
 	def get_shop_name(self):
 		return self.name
 
-shop = Shop('ice cream wonderland', {'chocolate' : 10, 'vanilla' : 12})
+	# Returns the price of the given item.
+	def get_item_price(self, item_name):
+		try:
+			return f'Price of item: {item_name}: ${self.catalog[item_name][1]}'
+		except:
+			print('Item does not exist.')
+
+	''' 
+	basic implementation of buying items, just looks at whether the item to be bought exist or not.
+	If the item is found, reduce the avaliable amount and return the updated catalog.
+	'''
+	def buy_item(self, item_name):
+		try:
+			if self.catalog[item_name][0] > 0:
+				self.catalog[item_name][0] = self.catalog[item_name][0] - 1
+				return self.catalog
+			else:
+				print(f'Item {item_name} is currently unavaliable.')
+				return self.catalog
+		except:
+			print('Invalid entry')
 
 
+shop = Shop('ice cream wonderland', {'dull sword' : [2, 10], 'broken shield' : [1, 10], 'cap' : [2, 10]})
+print(shop.get_item_price('dull sword'))
 
 
 
