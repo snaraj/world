6a08e4c688f1a2b519ff6b4ee40a8c2f9e469a06
User <user@Users-MacBook-Air.local>
2021-05-06 12:34:44 -0700

    adding 'health' base attribute to human ABC

--- humans/human.py
+++ humans/human.py
@@ -1,5 +1,5 @@
 '''
-Defines the blueprint for a human.
+	Defines the blueprint for a human.
 '''
 from abc import ABC, abstractmethod
 
@@ -10,9 +10,14 @@ from .inventory.inventory import Inventory
 class Human(ABC):
 
 	def __init__(self):
+		# Unique identifier generated per human, in place to avoid name confusions.
 		self.human_identifier = str(uuid.uuid4().int)
+		# Initialize a base attribute tree, which is inherited by Human subclasses.
 		self.human_attribute_tree = self.HumanAttributeTree().human_attribute_tree_dictionary
+		# Initialize a base Inventory which holds 4 spaces and its empty.
 		self.inventory = Inventory().content
+		# Human starts off with $0.
+		self.coins = 0
 
 	# Abstract getters common across all humans
 	@abstractmethod
@@ -34,18 +39,21 @@ class Human(ABC):
 
 		human_attribute_tree_dictionary = {
 			'intelligence' : 0,
-			'strength': 0
+			'strength': 0,
+			'health': 100
 		}
 
 		def __init__(self, 
 					intelligence = human_attribute_tree_dictionary['intelligence'], 
-					strength = human_attribute_tree_dictionary['strength']):
+					strength = human_attribute_tree_dictionary['strength'],
+					health = human_attribute_tree_dictionary['health']):
 
 			self.intelligence = intelligence
 			self.strength = strength
+			self.health = health
 
 		def __str__(self):
-			return 'Displaying human attribute tree: \n intelligence: {intelligence} - strength: {strength}'.format(**self._human_attribute_tree_dictionary)
+			return f'Displaying human attribute tree: intelligence: {self.intelligence} strength: {self.strength} health: {self.health}'
 
 		def __getitem__(self, attribute):
 			try: 
