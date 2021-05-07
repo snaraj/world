746914cc8aaccd45f0e588106d7e6e901e92dc63
snaranjo <unheroicmail@gmail.com>
2021-05-07 14:08:23 -0700

    Implementing __repr__ method, refactoring the loot table to be inside of __init__

--- bestiary/wolf.py
+++ bestiary/wolf.py
@@ -1,16 +1,18 @@
 class Wolf:
 	
-	loot_table = {
-		'bones' : 1,
-		'wolf tooth' : 5,
-		'wolf fur': 1
-	}
-
-	def __init__(self, health):
+	def __init__(self, health=50, dmg=5):
 		self.health = health
+		self.dmg = dmg
+		self.loot_table = {
+			'bones' : 1,
+			'wolf tooth' : 5,
+			'wolf fur': 1
+		}
 
+	# Unambigous Wolf Object representation.
+	def __repr__(self):
+		return 'Wolf(%r, %r, %r)' % (self.health, self.dmg, self.loot_table)
+
+	# Readable Wolf representation.
 	def __str__(self):
-		return f'Wolf has {self.health} health.'
-
-wolf = Wolf(120)
-print(wolf)
+		return 'It\'s a wolf!'
